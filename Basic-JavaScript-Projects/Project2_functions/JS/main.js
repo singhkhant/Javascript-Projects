7b46ae1625453fc195305a273f6b5ec4ba916989
singhkhant <manvir1141039@gmail.com>
2026-08-16 18:36:24 -0400

    Project2: Functions

---
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -0,0 +1,8 @@
+// Function that updates the HTML content
+function revealMessage() {
+    // Assigning two variables
+    var fact = "Welcome to the cosmos! ";                                               // Creating the Variable
+        fact += "Did you know that one million Earths could fit inside the Sun?";       // Using += operator to add to the variable
+        document.getElementById("CosmicText").innerHTML = fact;                         // Utilizing document.getElementById to change the text
+
+}
